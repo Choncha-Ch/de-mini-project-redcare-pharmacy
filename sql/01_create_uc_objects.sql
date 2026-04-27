51764e5c63b9484939de7e6f4ed5f616ba0bd43a
Choncha-Ch <baiferncc@gmail.com>
2026-04-27 09:51:05 +0000

    SQL updated to create Unity Catalog objects

---
+++ sql/01_create_uc_objects.sql
@@ -0,0 +1,4 @@
+CREATE CATALOG IF NOT EXISTS redcare_dev;
+CREATE SCHEMA IF NOT EXISTS redcare_dev.operations;
+CREATE VOLUME IF NOT EXISTS redcare_dev.operations.landing;
+CREATE VOLUME IF NOT EXISTS redcare_dev.operations.checkpoints;
